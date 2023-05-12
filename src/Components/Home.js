59f45c3be31550e3ecdb3001c52e670ba0d98161
Brian Dickson <brianlawrencedickson@gmail.com>
2023-05-12 16:08:30 +0100

    added buttons

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import NavBar from "./NavBar";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   const backgroundImage = {
@@ -20,7 +20,7 @@ const Home = () => {
     width: "700px",
     height: "700px",
     position: "absolute",
-    top: "50%",
+    top: "55%",
     left: "50%",
     transform: "translate(-50%, -50%)",
   };
@@ -59,11 +59,11 @@ const Home = () => {
       </div>
 
       <div className="px-20 bg-gray-200">
-        <div className="flex flex-col justify-center items-center text-6xl font-light italic p-10 bg-gray-200">
+        <div className="flex flex-col justify-center items-center text-6xl font-light italic p-20 y-10 bg-gray-200">
           What We Do
         </div>
 
-        <div className="grid grid-cols-3 grid-rows-4 bg-gray-200 justify-items-center align-items-center py-5">
+        {/* <div className="grid grid-cols-3 grid-rows-4 bg-gray-200 justify-items-center align-items-center py-5">
           <div className="col-start-1 row-start-3 text-4xl font-light italic text-center">
             Monetised Youtube
           </div>
@@ -73,7 +73,7 @@ const Home = () => {
           <div className="col-start-3 row-start-3 text-4xl font-light italic text-center">
             Sponsorship/ Patreon
           </div>
-        </div>
+        </div> */}
 
         <div className="grid grid-cols-3 grid-rows-2 bg-gray-200 justify-items-center align-items-center gap-8">
           <div
@@ -117,23 +117,63 @@ const Home = () => {
             made available to view on our channel. Through a monetised YouTube
             channel, funds can be generated from clicks and views, rather than
             donations.
+            <div className="flex flex-col justify-center items-center py-10">
+              <Link className="text-3xl font-light italic" to="/youtube">
+                <a
+                  href="#_"
+                  class="relative inline-block px-4 py-2 font-medium group"
+                >
+                  <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                  <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+                  <span class="relative text-black group-hover:text-white">
+                    Youtube
+                  </span>
+                </a>
+              </Link>
+            </div>
           </div>
-          <div className="col-start-2 row-start-2 text-center text-lg">
+          <div className="col-start-2 row-start-2 text-center text-lg ">
             For every YouTube video we publish, there will also be a pinhole
             camera photograph taken from the same vantage point. These will then
             be screenprinted using traditional techniques at The Glasgow Print
             Studio.
+            <div className="flex flex-col justify-center items-center py-10">
+              <Link className="text-3xl font-light italic" to="/screenprints">
+                <a
+                  href="#_"
+                  class="relative inline-block px-4 py-2 font-medium group"
+                >
+                  <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                  <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+                  <span class="relative text-black group-hover:text-white">
+                    Screenprints
+                  </span>
+                </a>
+              </Link>
+            </div>
           </div>
           <div className="col-start-3 row-start-2 text-center text-lg">
             As we grow, you may wish to have your relevant products featured or
             discussed on our online platforms. Simply contact us via email.
             Also, Anyone wishing to donate to The Coorie Project need only visit
             our Patreon account .
+            <div className="flex flex-col justify-center items-center py-10">
+              <Link className="text-3xl font-light italic" to="/screenprints">
+                <a
+                  href="#_"
+                  class="relative inline-block px-4 py-2 font-medium group"
+                >
+                  <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                  <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+                  <span class="relative text-black group-hover:text-white">
+                    Patreon
+                  </span>
+                </a>
+              </Link>
+            </div>
           </div>
-          
         </div>
       </div>
-
     </body>
   );
 };
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,27 +1,77 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 const NavBar = () => {
-
   return (
-    <nav >
-    <ul class="flex space-x-20 justify-center p-20" id= "navbar">
-      <li>
-        <Link class="nline-block rounded bg-neutral-800 px-3 pb-2 pt-2.5 text-xl font-medium leading-normal text-neutral-50 shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] transition duration-150 ease-in-out hover:bg-neutral-800 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-neutral-800 focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] dark:bg-neutral-700 dark:shadow-[0_4px_9px_-4px_#030202] dark:hover:bg-neutral-800 dark:hover:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:focus:bg-neutral-900 dark:focus:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:active:bg-neutral-900 dark:active:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)]" to="/">Home</Link>
-      </li>
-      <li>
-        <Link class="nline-block rounded bg-neutral-800 px-3 pb-2 pt-2.5 text-xl font-medium leading-normal text-neutral-50 shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] transition duration-150 ease-in-out hover:bg-neutral-800 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-neutral-800 focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] dark:bg-neutral-700 dark:shadow-[0_4px_9px_-4px_#030202] dark:hover:bg-neutral-800 dark:hover:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:focus:bg-neutral-900 dark:focus:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:active:bg-neutral-900 dark:active:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)]" to="/mission-statement">Mission Statement</Link>
-      </li>
-      <li>
-        <Link class="nline-block rounded bg-neutral-800 px-3 pb-2 pt-2.5 text-xl font-medium leading-normal text-neutral-50 shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] transition duration-150 ease-in-out hover:bg-neutral-800 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-neutral-800 focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] dark:bg-neutral-700 dark:shadow-[0_4px_9px_-4px_#030202] dark:hover:bg-neutral-800 dark:hover:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:focus:bg-neutral-900 dark:focus:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:active:bg-neutral-900 dark:active:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)]" to="/screenprints">Screenprints</Link>
-      </li>
-      <li>
-        <Link class="nline-block rounded bg-neutral-800 px-3 pb-2 pt-2.5 text-xl font-medium leading-normal text-neutral-50 shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] transition duration-150 ease-in-out hover:bg-neutral-800 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-neutral-800 focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] dark:bg-neutral-700 dark:shadow-[0_4px_9px_-4px_#030202] dark:hover:bg-neutral-800 dark:hover:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:focus:bg-neutral-900 dark:focus:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:active:bg-neutral-900 dark:active:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)]" to="/youtube">Youtube</Link>
-      </li>
-    </ul>
+    <nav>
+      <ul class="flex space-x-20 justify-center p-20" id="navbar">
+        <li>
+          <div className="flex flex-col justify-center items-center py-10">
+            <Link className="text-2xl font-light italic" to="/">
+              <a
+                href="#_"
+                class="relative inline-block px-4 py-2 font-medium group"
+              >
+                <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+                <span class="relative text-black group-hover:text-white">
+                  Home
+                </span>
+              </a>
+            </Link>
+          </div>
+        </li>
+        <li>
+          <div className="flex flex-col justify-center items-center py-10">
+            <Link className="text-2xl font-light italic" to="/screenprints">
+              <a
+                href="#_"
+                class="relative inline-block px-4 py-2 font-medium group"
+              >
+                <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+                <span class="relative text-black group-hover:text-white">
+                  Screenprints
+                </span>
+              </a>
+            </Link>
+          </div>{" "}
+        </li>
+        <li>
+          <div className="flex flex-col justify-center items-center py-10">
+            <Link className="text-2xl font-light italic" to="/youtube">
+              <a
+                href="#_"
+                class="relative inline-block px-4 py-2 font-medium group"
+              >
+                <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+                <span class="relative text-black group-hover:text-white">
+                  Youtube
+                </span>
+              </a>
+            </Link>
+          </div>{" "}
+        </li>
+        <li>
+          <div className="flex flex-col justify-center items-center py-10">
+            <Link className="text-2xl font-light italic" to="/mission-statement">
+              <a
+                href="#_"
+                class="relative inline-block px-4 py-2 font-medium group"
+              >
+                <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+                <span class="relative text-black group-hover:text-white">
+                  MissionStatement
+                </span>
+              </a>
+            </Link>
+          </div>
+        </li>
+      </ul>
     </nav>
   );
-}
+};
 
 export default NavBar;
