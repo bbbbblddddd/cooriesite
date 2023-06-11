373acc171692ef3acfff23472ed9516b87b4423b
Brian Dickson <brianlawrencedickson@gmail.com>
2023-06-11 11:42:08 +0100

    chaned main

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -41,7 +41,7 @@ const Home = () => {
     width: "700px",
     height: "700px",
     position: "absolute",
-    top: "calc(50% + 150px)", 
+    top: "calc(50% + 150px)",
     left: "50%",
     transform: "translate(-50%, -70px)",
   };
@@ -55,56 +55,75 @@ const Home = () => {
 
   return (
     <section>
-      <div className="relative bg-cover" style={{ ...waterfallImage, ...containerStyle }}>
+      <div
+        className="relative bg-cover"
+        style={{ ...waterfallImage, ...containerStyle }}
+      >
         <NavBar className="absolute top-0 left-0 right-0" />
         <div className="parallax text-center mt-10" ref={logoRef}>
           <div style={coorieLogo}></div>
         </div>
       </div>
 
-
-
-
-
-
-
-
-
-
-
+      <div>
+        <h1 class="text-5xl text-gray-700 text-center p-20">
+          COORIE / ku:ri /{" "}
+        </h1>
+        <h1 class="text-5xl text-gray-700 text-center p-20">
+          The Scottish art of deriving comfort, wellbeing and energy from wild
+          landscapes.
+        </h1>
+        <h1 class="text-5xl text-gray-700 text-center p-20">
+          The aim of The Coorie Project is to create an online platform to
+          highlight the beauty of the natural world, and help protect it by
+          raising awareness and funds for climate change science and curated
+          sustainability projects.
+        </h1>
+      </div>
 
       <div className="flex flex-col items-center justify-center h-screen">
-  <div className="flex justify-between mb-4">
-    <div className="flex flex-col items-center w-full">
-      <h1 className="text-xl">Title 3</h1>
-      <div className="px-10">
-        <img className="w-full" src="https://coorieprojectimagesbbbbblddddd.s3.eu-west-2.amazonaws.com/lIekaw4+-+Imgur.jpg" alt="explainer 1" />
+        <div className="flex justify-between mb-4">
+          <div className="flex flex-col items-center w-full">
+            <h1 className="text-xl">Title 3</h1>
+            <div className="px-10">
+              <img
+                className="w-full"
+                src="https://coorieprojectimagesbbbbblddddd.s3.eu-west-2.amazonaws.com/lIekaw4+-+Imgur.jpg"
+                alt="explainer 1"
+              />
+            </div>
+            <a className="px-4 py-2 bg-gray-300 text-black" href="#">
+              Button 1
+            </a>
+          </div>
+          <div className="flex flex-col items-center w-full">
+            <h1 className="text-xl">Title 3</h1>
+            <div className="px-10">
+              <img
+                className="w-full"
+                src="https://coorieprojectimagesbbbbblddddd.s3.eu-west-2.amazonaws.com/lIekaw4+-+Imgur.jpg"
+                alt="explainer 2"
+              />
+            </div>
+            <a className="px-4 py-2 bg-gray-300 text-black" href="#">
+              Button 2
+            </a>
+          </div>
+          <div className="flex flex-col items-center w-full">
+            <h1 className="text-xl">Title 3</h1>
+            <div className="px-10">
+              <img
+                className="w-full"
+                src="https://coorieprojectimagesbbbbblddddd.s3.eu-west-2.amazonaws.com/lIekaw4+-+Imgur.jpg"
+                alt="explainer3"
+              />
+            </div>
+            <a className="px-4 py-2 bg-gray-300 text-black" href="#">
+              Button 3
+            </a>
+          </div>
+        </div>
       </div>
-      <a className="px-4 py-2 bg-gray-300 text-black" href="#">Button 1</a>
-    </div>
-    <div className="flex flex-col items-center w-full">
-      <h1 className="text-xl">Title 3</h1>
-      <div className="px-10">
-        <img className="w-full" src="https://coorieprojectimagesbbbbblddddd.s3.eu-west-2.amazonaws.com/lIekaw4+-+Imgur.jpg" alt="explainer 2" />
-      </div>
-      <a className="px-4 py-2 bg-gray-300 text-black" href="#">Button 2</a>
-    </div>
-    <div className="flex flex-col items-center w-full">
-      <h1 className="text-xl">Title 3</h1>
-      <div className="px-10">
-        <img className="w-full" src="https://coorieprojectimagesbbbbblddddd.s3.eu-west-2.amazonaws.com/lIekaw4+-+Imgur.jpg" alt="explainer3" />
-      </div>
-      <a className="px-4 py-2 bg-gray-300 text-black" href="#">Button 3</a>
-    </div>
-  </div>
-</div>
-
-
-
-
-
-
-
     </section>
   );
 };
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,20 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 
 const NavBar = () => {
+  const isSmallScreen = useMediaQuery({ maxWidth: 768 });
+
   return (
     <nav>
-      <ul class="flex space-x-20 justify-center p-20" id="navbar">
+      <ul
+        className={`flex ${isSmallScreen ? "flex-col" : "space-x-20"} justify-center p-20`}
+        id="navbar"
+      >
         <li>
           <div className="flex flex-col justify-center items-center py-10">
             <Link className="text-2xl font-light italic" to="/">
               <a
                 href="#_"
-                class="relative inline-block px-4 py-2 font-medium group"
+                className="relative inline-block px-4 py-2 font-medium group"
               >
-                <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-                <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
-                <span class="relative text-black group-hover:text-white">
+                <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+                <span className="relative text-black group-hover:text-white">
                   Home
                 </span>
               </a>
@@ -26,11 +32,11 @@ const NavBar = () => {
             <Link className="text-2xl font-light italic" to="/screenprints">
               <a
                 href="#_"
-                class="relative inline-block px-4 py-2 font-medium group"
+                className="relative inline-block px-4 py-2 font-medium group"
               >
-                <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-                <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
-                <span class="relative text-black group-hover:text-white">
+                <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+                <span className="relative text-black group-hover:text-white">
                   Screenprints
                 </span>
               </a>
@@ -42,34 +48,33 @@ const NavBar = () => {
             <Link className="text-2xl font-light italic" to="/youtube">
               <a
                 href="#_"
-                class="relative inline-block px-4 py-2 font-medium group"
+                className="relative inline-block px-4 py-2 font-medium group"
               >
-                <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-                <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
-                <span class="relative text-black group-hover:text-white">
+                <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+                <span className="relative text-black group-hover:text-white">
                   Youtube
                 </span>
               </a>
             </Link>
           </div>{" "}
         </li>
-  
-        <div className="flex flex-col justify-center items-center py-10">
+        <li>
+          <div className="flex flex-col justify-center items-center py-10">
             <Link className="text-2xl font-light italic" to="/radio">
               <a
                 href="#_"
-                class="relative inline-block px-4 py-2 font-medium group"
+                className="relative inline-block px-4 py-2 font-medium group"
               >
-                <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-                <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
-                <span class="relative text-black group-hover:text-white">
+                <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+                <span className="relative text-black group-hover:text-white">
                   Radio Show
                 </span>
               </a>
             </Link>
           </div>
-
-      
+        </li>
       </ul>
     </nav>
   );
