3aac2d896209537dd21cc6a0c6beeaafcdef0287
Brian Dickson <brianlawrencedickson@gmail.com>
2023-06-14 15:13:59 +0100

    added navbar styling and react responsiveness on home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import NavBar from "./NavBar";
-// import { Link } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 
 const Home = () => {
   const logoRef = useRef(null);
@@ -53,78 +53,106 @@ const Home = () => {
     alignItems: "center",
   };
 
+  const breakpoints = {
+    small: "(max-width: 639px)",
+    medium: "(min-width: 640px) and (max-width: 1023px)",
+    large: "(min-width: 1024px)",
+  };
+
+  const isSmallScreen = useMediaQuery({ query: breakpoints.small });
+  // const isMediumScreen = useMediaQuery({ query: breakpoints.medium });
+  // const isLargeScreen = useMediaQuery({ query: breakpoints.large });
+
+  const waterfallImageStyle = {
+    ...waterfallImage,
+    ...containerStyle,
+    height: isSmallScreen ? "800px" : "1200px",
+  };
+
+  const coorieLogoStyle = {
+    ...coorieLogo,
+    top: isSmallScreen ? "calc(50% + 100px)" : "calc(50% + 150px)",
+    height: isSmallScreen ? "400px" : "700px",
+  };
+
+  const pageContainerStyle = {
+    maxWidth: "100%",
+    overflowX: "hidden",
+  };
+
+  const titleStyle = {
+    fontSize: isSmallScreen ? "1rem" : "3rem",
+  };
+
   return (
     <section>
-      <div
-        className="relative bg-cover"
-        style={{ ...waterfallImage, ...containerStyle }}
-      >
+      <div className="relative bg-cover" style={waterfallImageStyle}>
         <NavBar className="absolute top-0 left-0 right-0" />
         <div className="parallax text-center mt-10" ref={logoRef}>
-          <div style={coorieLogo}></div>
+          <div style={coorieLogoStyle}></div>
         </div>
       </div>
 
-      <div>
-        <h1 class="text-5xl text-gray-700 text-center p-20">
-          COORIE / ku:ri /{" "}
+      <div style={pageContainerStyle}>
+        <h1 className="text-5xl text-gray-700 text-center p-20" style={titleStyle}>
+          COORIE / ku:ri /
         </h1>
-        <h1 class="text-5xl text-gray-700 text-center p-20">
-          The Scottish art of deriving comfort, wellbeing and energy from wild
-          landscapes.
+        <h1 className="text-5xl text-gray-700 text-center p-20" style={titleStyle}>
+          The Scottish art of deriving comfort, wellbeing and energy from wild landscapes.
         </h1>
-        <h1 class="text-5xl text-gray-700 text-center p-20">
-          The aim of The Coorie Project is to create an online platform to
-          highlight the beauty of the natural world, and help protect it by
-          raising awareness and funds for climate change science and curated
-          sustainability projects.
+        <h1 className="text-5xl text-gray-700 text-center p-20" style={titleStyle}>
+          The aim of The Coorie Project is to create an online platform to highlight the beauty of the natural world, and help protect it by raising awareness and funds for climate change science and curated sustainability projects.
         </h1>
-      </div>
 
-      <div className="flex flex-col items-center justify-center h-screen">
-        <div className="flex justify-between mb-4">
-          <div className="flex flex-col items-center w-full">
-            <h1 className="text-xl">Title 3</h1>
-            <div className="px-10">
-              <img
-                className="w-full"
-                src="https://coorieprojectimagesbbbbblddddd.s3.eu-west-2.amazonaws.com/lIekaw4+-+Imgur.jpg"
-                alt="explainer 1"
-              />
+        <div className="flex flex-col items-center justify-center h-screen">
+          <div
+            className={`flex ${isSmallScreen ? "flex-col" : "justify-between"} mb-4`}
+          >
+            <div className="flex flex-col items-center w-full">
+              <h1 className="text-xl">Title 3</h1>
+              <div className="px-10">
+                <img
+                  className="w-full"
+                  src="https://coorieprojectimagesbbbbblddddd.s3.eu-west-2.amazonaws.com/lIekaw4+-+Imgur.jpg"
+                  alt="explainer 1"
+                />
+              </div>
+              <a className="px-4 py-2 bg-gray-300 text-black" href="#">
+                Button 1
+              </a>
             </div>
-            <a className="px-4 py-2 bg-gray-300 text-black" href="#">
-              Button 1
-            </a>
-          </div>
-          <div className="flex flex-col items-center w-full">
-            <h1 className="text-xl">Title 3</h1>
-            <div className="px-10">
-              <img
-                className="w-full"
-                src="https://coorieprojectimagesbbbbblddddd.s3.eu-west-2.amazonaws.com/lIekaw4+-+Imgur.jpg"
-                alt="explainer 2"
-              />
+            <div className="flex flex-col items-center w-full">
+              <h1 className="text-xl">Title 3</h1>
+              <div className="px-10">
+                <img
+                  className="w-full"
+                  src="https://coorieprojectimagesbbbbblddddd.s3.eu-west-2.amazonaws.com/lIekaw4+-+Imgur.jpg"
+                  alt="explainer 2"
+                />
+              </div>
+              <a className="px-4 py-2 bg-gray-300 text-black" href="#">
+                Button 2
+              </a>
             </div>
-            <a className="px-4 py-2 bg-gray-300 text-black" href="#">
-              Button 2
-            </a>
-          </div>
-          <div className="flex flex-col items-center w-full">
-            <h1 className="text-xl">Title 3</h1>
-            <div className="px-10">
-              <img
-                className="w-full"
-                src="https://coorieprojectimagesbbbbblddddd.s3.eu-west-2.amazonaws.com/lIekaw4+-+Imgur.jpg"
-                alt="explainer3"
-              />
+            <div className="flex flex-col items-center w-full">
+              <h1 className="text-xl">Title 3</h1>
+              <div className="px-10">
+                <img
+                  className="w-full"
+                  src="https://coorieprojectimagesbbbbblddddd.s3.eu-west-2.amazonaws.com/lIekaw4+-+Imgur.jpg"
+                  alt="explainer3"
+                />
+              </div>
+              <a className="px-4 py-2 bg-gray-300 text-black" href="#">
+                Button 3
+              </a>
             </div>
-            <a className="px-4 py-2 bg-gray-300 text-black" href="#">
-              Button 3
-            </a>
           </div>
         </div>
       </div>
     </section>
   );
 };
+
 export default Home;
+
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,81 +1,111 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useMediaQuery } from "react-responsive";
 
 const NavBar = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
   const isSmallScreen = useMediaQuery({ maxWidth: 768 });
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
 
   return (
     <nav>
-      <ul
-        className={`flex ${isSmallScreen ? "flex-col" : "space-x-20"} justify-center p-20`}
-        id="navbar"
-      >
-        <li>
-          <div className="flex flex-col justify-center items-center py-10">
-            <Link className="text-2xl font-light italic" to="/">
-              <a
-                href="#_"
-                className="relative inline-block px-4 py-2 font-medium group"
+      <div className="fixed top-0 left-0 z-50 w-full">
+        <div className={`flex justify-between items-center px-4 py-2 ${isMenuOpen ? "bg-white bg-opacity-90" : ""}`}>
+          <div
+            className={`cursor-pointer ${
+              isMenuOpen ? "text-black" : "text-gray-500"
+            }`}
+            onClick={toggleMenu}
+          >
+            {isMenuOpen ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
               >
-                <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-                <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
-                <span className="relative text-black group-hover:text-white">
-                  Home
-                </span>
-              </a>
-            </Link>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <img
+                src="/images/hamburger.png"
+                alt="Hamburger Logo"
+                className="h-12 w-12"
+              />
+            )}
           </div>
-        </li>
-        <li>
-          <div className="flex flex-col justify-center items-center py-10">
-            <Link className="text-2xl font-light italic" to="/screenprints">
-              <a
-                href="#_"
-                className="relative inline-block px-4 py-2 font-medium group"
-              >
-                <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-                <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
-                <span className="relative text-black group-hover:text-white">
-                  Screenprints
-                </span>
-              </a>
-            </Link>
-          </div>{" "}
-        </li>
-        <li>
-          <div className="flex flex-col justify-center items-center py-10">
-            <Link className="text-2xl font-light italic" to="/youtube">
-              <a
-                href="#_"
-                className="relative inline-block px-4 py-2 font-medium group"
-              >
-                <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-                <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
-                <span className="relative text-black group-hover:text-white">
-                  Youtube
-                </span>
-              </a>
-            </Link>
-          </div>{" "}
-        </li>
-        <li>
-          <div className="flex flex-col justify-center items-center py-10">
-            <Link className="text-2xl font-light italic" to="/radio">
-              <a
-                href="#_"
-                className="relative inline-block px-4 py-2 font-medium group"
-              >
-                <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-                <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
-                <span className="relative text-black group-hover:text-white">
-                  Radio Show
-                </span>
-              </a>
-            </Link>
-          </div>
-        </li>
-      </ul>
+        </div>
+        {isMenuOpen && (
+          <ul className="flex flex-col justify-center items-center p-20 space-y-10 bg-white bg-opacity-90">
+            <li>
+              <Link className="text-2xl font-light italic" to="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link className="text-2xl font-light italic" to="/screenprints">
+                Screenprints
+              </Link>
+            </li>
+            <li>
+              <Link className="text-2xl font-light italic" to="/youtube">
+                Youtube
+              </Link>
+            </li>
+            <li>
+              <Link className="text-2xl font-light italic" to="/radio">
+                Radio Show
+              </Link>
+            </li>
+            <div className="flex justify-center space-x-10">
+              <li>
+                <a
+                  href="https://www.youtube.com/channel/UCULPVxk5Zjk2zje5drAou_Q?app=desktop&disable_polymer=true"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src="/images/youtube.png"
+                    alt="YouTube Logo"
+                    className="h-6 w-6 cursor-pointer"
+                  />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.instagram.com/coorieproject/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src="/images/instagram.png"
+                    alt="Instagram Logo"
+                    className="h-6 w-6 cursor-pointer"
+                  />
+                </a>
+              </li>
+              <li>
+                <a href="mailto:coorieproject@gmail.com">
+                  <img
+                    src="/images/email.png"
+                    alt="Email Logo"
+                    className="h-6 w-6 cursor-pointer"
+                  />
+                </a>
+              </li>
+            </div>
+          </ul>
+        )}
+      </div>
     </nav>
   );
 };
