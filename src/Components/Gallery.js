1d843c28a9f38d33b532f18b88b9a9b6ecd377b4
Brian Dickson <brianlawrencedickson@gmail.com>
2023-06-19 10:07:13 +0100

    added h1 tags to gallery

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -28,7 +28,6 @@ const Gallery = () => {
       },
       { threshold: 0.2 }
     );
-    
 
     const gallerySections = document.querySelectorAll(".gallery-section");
     gallerySections.forEach((section) => {
@@ -45,47 +44,8 @@ const Gallery = () => {
     };
   }, []);
 
-  
-
   return (
     <div class="align-items-center text-center justify-center items-center">
-      <section
-        className="gallery-section"
-        data-image="print1"
-        ref={useRef(null)}
-      >
-        <img
-          src="/images/print1.png"
-          alt="Print 1"
-          className={`gallery-image ${imageVisible["print1"] ? "show" : ""}`}
-        />
-         <br />
-         <h1 class={`text-6xl ${isSmallScreen ? 'text-xl' : 'text-customGray'} font-light text-center px-6`}>
-  The Giant's Causeway
-</h1>
-
-        <br />
-        <br />
-        <h1
-  className={`text-4xl ${
-    isSmallScreen ? 'text-sm' : 'text-customGray'
-  } font-light italic text-center ${
-    isSmallScreen ? 'px-8' : 'px-32'
-  }`}
->
-  This image was captured using a pinhole camera at The Giants Causeway
-  in Country Antrim, Northern Ireland. While the Coorie Project aims to
-  highlight the beauty of the natural world here in Scotland, a trip to
-  the Giant’s Causeway while in Belfast could not be ignored. After all,
-  the very same polygonal rock formations appear in Fingal’s Cave off
-  the west coast of Scotland on the Isle of Staffa, formed by the same
-  ancient lava flow. This location, teaming with tourists, was able to
-  be captured through a combination of luck, timing and a reckless
-  disregard for the artist’s own personal safety.
-</h1>
-
-      </section>
-
       <section
         className="gallery-section"
         data-image="print2"
@@ -96,27 +56,28 @@ const Gallery = () => {
           alt="Print 2"
           className={`gallery-image ${imageVisible["print2"] ? "show" : ""}`}
         />
-        <h1 class="text-3xl text-gray-700 text-center">
-          {" "}
-          The Giant's Causeway
+        <br />
+        <h1
+          class={`text-6xl ${
+            isSmallScreen ? "text-xl" : "text-customGray"
+          } font-light text-center px-6`}
+        >
+          The Devils Pulpit
+        </h1>
+        <br />
+        <br />
+        <h1
+          className={`text-4xl ${
+            isSmallScreen ? "text-sm" : "text-customGray"
+          } font-light italic text-center ${isSmallScreen ? "px-8" : "px-32"}`}
+        >
+          This image was captured using a pinhole camera at the Devil’s Pulpit
+          in Dumgoyne, Scotland. Due to the low light conditions at the Devils
+          Pulpit, a long exposure of around six minutes was necessary, creating
+          the sense of movement from the waterfalls captured.
         </h1>
       </section>
-
-      <section
-        className="gallery-section"
-        data-image="print3"
-        ref={useRef(null)}
-      >
-        <img
-          src="/images/print3.png"
-          alt="Print 3"
-          className={`gallery-image ${imageVisible["print3"] ? "show" : ""}`}
-        />
-        <h1 class="text-3xl text-gray-700 text-center">
-          {" "}
-          The Giant's Causeway
-        </h1>
-      </section>
+      <br />
 
       <section
         className="gallery-section"
@@ -128,11 +89,27 @@ const Gallery = () => {
           alt="Print 4"
           className={`gallery-image ${imageVisible["print4"] ? "show" : ""}`}
         />
-        <h1 class="text-3xl text-gray-700 text-center">
-          {" "}
-          The Giant's Causeway
+        <br />
+        <h1
+          class={`text-6xl ${
+            isSmallScreen ? "text-xl" : "text-customGray"
+          } font-light text-center px-6`}
+        >
+          Arran Forest
+        </h1>
+        <br />
+        <br />
+        <h1
+          className={`text-4xl ${
+            isSmallScreen ? "text-sm" : "text-customGray"
+          } font-light italic text-center ${isSmallScreen ? "px-8" : "px-32"}`}
+        >
+          This image was captured on the Isle of Arran, often referred to as "Scotland in Miniature". This image shows a
+          section of forest up the trail that leads to the highest point on the
+          island, the mountain Goatfell. The image depicts a scene of a forest, with a section of fallen trees.
         </h1>
       </section>
+       <br />
 
       <section
         className="gallery-section"
@@ -144,9 +121,96 @@ const Gallery = () => {
           alt="Print 5"
           className={`gallery-image ${imageVisible["print5"] ? "show" : ""}`}
         />
-        <h1 class="text-3xl text-gray-700 text-center">
-          {" "}
+        <br />
+        <h1
+          class={`text-6xl ${
+            isSmallScreen ? "text-xl" : "text-customGray"
+          } font-light text-center px-6`}
+        >
+          The Falls of Falloch
+        </h1>
+        <br />
+        <br />
+        <h1
+          className={`text-4xl ${
+            isSmallScreen ? "text-sm" : "text-customGray"
+          } font-light italic text-center ${isSmallScreen ? "px-8" : "px-32"}`}
+        >
+          This image was captured using a pinhole camera at the The Falls of
+          Falloch at the northernmost tip of Loch Lomond. This involved a sixty
+          mile round trip cycle from Balloch. This location also goes by the
+          name ‘Rob Roy’s Bathtub’ and is renowned for being one of the best
+          enclosed spots for wild swimming in Scotland. Fantastic light
+          conditions on the day meant a fairly short exposure time was required
+          to catch the movement of the waterfall.
+        </h1>
+      </section>
+      <br />
+
+      <section
+        className="gallery-section"
+        data-image="print1"
+        ref={useRef(null)}
+      >
+        <img
+          src="/images/print1.png"
+          alt="Print 1"
+          className={`gallery-image ${imageVisible["print1"] ? "show" : ""}`}
+        />
+        <br />
+        <h1
+          class={`text-6xl ${
+            isSmallScreen ? "text-xl" : "text-customGray"
+          } font-light text-center px-6`}
+        >
           The Giant's Causeway
+        </h1>
+        <br />
+        <br />
+        <h1
+          className={`text-4xl ${
+            isSmallScreen ? "text-sm" : "text-customGray"
+          } font-light italic text-center ${isSmallScreen ? "px-8" : "px-32"}`}
+        >
+          This image was captured using a pinhole camera at The Giants Causeway
+          in Country Antrim, Northern Ireland. While the Coorie Project aims to
+          highlight the beauty of the natural world here in Scotland, a trip to
+          the Giant’s Causeway while in Belfast could not be ignored.
+        </h1>
+      </section>
+      <br />
+
+      <section
+        className="gallery-section"
+        data-image="print3"
+        ref={useRef(null)}
+      >
+        <img
+          src="/images/print3.png"
+          alt="Print 3"
+          className={`gallery-image ${imageVisible["print3"] ? "show" : ""}`}
+        />
+        <br />
+        <h1
+          class={`text-6xl ${
+            isSmallScreen ? "text-xl" : "text-customGray"
+          } font-light text-center px-6`}
+        >
+          The Giant's Causeway
+        </h1>
+        <br />
+        <br />
+        <h1
+          className={`text-4xl ${
+            isSmallScreen ? "text-sm" : "text-customGray"
+          } font-light italic text-center ${isSmallScreen ? "px-8" : "px-32"}`}
+        >
+          Another image from my trip to The Giants Causeway. The polygonal rock
+          formations captured here stretch across the Irish Sea and reappear in
+          Fingal’s Cave off the west coast of Scotland on the Isle of Staffa,
+          formed by the same ancient lava flow. This location, teaming with
+          tourists, was able to be captured through a combination of luck,
+          timing and a reckless disregard for the artist’s own personal safety.
         </h1>
       </section>
     </div>
